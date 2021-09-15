8013e5a374cc8495031646510d23c943c48648eb
hansonh <hansonh@localhost>
2021-09-15 13:18:26 -0400

    hi

--- Java I Class/src/JavaClass/Class.java
+++ Java I Class/src/JavaClass/Class.java
@@ -3,9 +3,16 @@ package JavaClass;
 public class Class{
 	
 	public static void main(String[] args) {
-		
-		System.out.println("hello");
-		
+		System.out.println("Which dorm do you live in?");
+		System.out.println("I live in Theriot South.");
+		System.out.println("Where are you from?");
+		System.out.println("I am from China.");
+		System.out.println("What is your favorite sport?");
+		System.out.println("My favorite sport is soccer.");
+		System.out.println("What is your favorite class?");
+		System.out.println("My favorite class is Computer Science.");
+		System.out.println("What did you do for summer?");
+		System.out.println("I went back to China and took some classes, including some on C++.");
 	}
 	
 }
