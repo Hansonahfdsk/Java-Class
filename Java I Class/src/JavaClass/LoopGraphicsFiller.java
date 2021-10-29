c767657bb6f856e726839dc0ae2488ee512cbe33
hansonh <hansonh@localhost>
2021-10-28 20:40:06 -0400

    Graphical Loops

---
+++ Java I Class/src/JavaClass/LoopGraphicsFiller.java
@@ -0,0 +1,77 @@
+// filler code for pong provided by Mr. David
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LoopGraphicsFiller extends JPanel {
+	
+	// constants that are predefined and won't change as the program runs
+	private final int WIDTH = 1000, HEIGHT = 590;
+
+	// defines what we want to happen anytime we draw the graphics
+	public void paint(Graphics g) {
+		
+		// background color is gray
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		// call your methods here
+		
+		
+	}
+
+	//////////////////////////////////////
+	//////////////////////////////////////
+	
+	// DON'T TOUCH THE BELOW CODE
+	
+	
+	// this method runs the actual program.
+	public void run() {
+
+		// while(true) should rarely be used to avoid infinite loops, but here it is ok because
+		// closing the graphics window will end the program
+		while (true) {
+	
+			// draws
+			//repaint();
+			
+			//rests for a hundredth of a second
+			try {
+				Thread.sleep(10);
+			} catch (Exception ex) {}
+		}
+	}
+	
+	// very simple main method to get the game going
+	public static void main(String[] args) {
+		new GraphicsLoops();
+	}
+ 
+	// does complicated stuff to initialize the graphics and key listeners
+	// DO NOT CHANGE THIS CODE UNLESS YOU ARE EXPERIENCED WITH JAVA
+	// GRAPHICS!
+	public LoopGraphicsFiller() {
+		JFrame frame = new JFrame();
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(this);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		this.setFocusable(true);
+		
+		run();
+	}
+}
