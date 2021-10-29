aa07a3f8276378ffa0f927d6b6c20b3fbce842eb
hansonh <hansonh@localhost>
2021-10-28 20:41:54 -0400

    Graphicalloops

--- Java I Class/src/JavaClass/LoopGraphicsFiller.java
+++ Java I Class/src/JavaClass/LoopGraphicsFiller.java
@@ -1,5 +1,4 @@
-// filler code for pong provided by Mr. David
-
+package JavaClass;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -18,15 +17,79 @@ public class LoopGraphicsFiller extends JPanel {
 	
 	// constants that are predefined and won't change as the program runs
 	private final int WIDTH = 1000, HEIGHT = 590;
-
+	
+	public void ncircles(int n, Graphics g) {
+		int x = 0, y = 0, diam = 5;
+		g.setColor(Color.black);
+		for (int i = 0; i < n; i++) {
+			g.fillOval(x, y, diam, diam);
+			x += 5;
+			y += 5;
+		}
+	}
+	public void sbs(Graphics g) {
+		int x = 0, y = HEIGHT / 2, diam = 5;
+		g.setColor(Color.black);
+		for (int i = 0; i < 10; i++) {
+			g.fillOval(x, y, diam, diam);
+			x += 5;
+			if (diam <= 50) {
+				diam += 5;
+			}
+			else {
+				diam -= 5;
+			}
+		}
+	}
+	public void tenbyten(Graphics g) {
+		int x = 0, y = 0, diam = 5;
+		g.setColor(Color.black);
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				g.fillOval(x, y, diam, diam);
+				x += 5;
+			}
+			y += HEIGHT / 10;
+		}
+	}
+	public void checker(Graphics g) {
+		int x = 0, y = 0, len = WIDTH / 10;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				g.fillRect(x, y, len, len);
+				x += WIDTH / 10;
+			}
+			y += WIDTH / 10;
+		}
+	}
+	public void tricirsqu(Graphics g) {
+		int x = 0, y = 0, len = 5, shape = 1;
+		for (int i = 0; i < 5; i++) {
+			if (shape == 1) {
+				g.fillTriangle(x, y, len);
+				shape = 2;
+			}
+			if (shape == 2) {
+				g.fillOval(x, y, len, len);
+				shape = 3;
+			}
+			if (shape = 3) {
+				g.fillRect(x, y, len, len);
+				shape = 1;
+			}
+		}
+	}
 	// defines what we want to happen anytime we draw the graphics
 	public void paint(Graphics g) {
 		
 		// background color is gray
-		g.setColor(Color.LIGHT_GRAY);
+		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
-		// call your methods here
+		ncircles(5);
+		sbs();
+		tenbyten();
+		checker();
+		tricirsqu();
 		
 		
 	}
