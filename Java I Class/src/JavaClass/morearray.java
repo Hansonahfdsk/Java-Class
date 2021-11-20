6c19ec14748a4cb2fa839c90304ed312de4f4c45
hansonh <hansonh@hansons-mbp.lan>
2021-11-20 17:08:13 -0500

    morearraypractice

---
+++ Java I Class/src/JavaClass/morearray.java
@@ -0,0 +1,18 @@
+package JavaClass;
+
+public class morearray {
+	
+	public void display(int[] arr){
+		string[] arr
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		
+		morearray tester = new morearray();
+		tester.display(new int[] {1, 2, 3});
+	}
+}
