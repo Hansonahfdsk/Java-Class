5de5a3b9f6d663d84356c8193c50e53f9c53f4bd
hansonh <hansonh@localhost>
2022-01-25 15:58:31 -0500

    Humanclass

--- Java I Class/src/JavaClass/Human.java
+++ Java I Class/src/JavaClass/Human.java
@@ -1,67 +1,31 @@
 package JavaClass;
+
 public class Human {
-	
-	int age;
-	char gender;
-	
-	public void setAge (int a) {
-		age = a;
+	private int age, height;
+	private String name;
+	public Human(int a, int h, String n) {
+		this.age = a;
+		this.height = h;
+		this.name = n;
 	}
-	public void setGender (char b) {
-		gender = b;
+	public void getOlder() {
+		age++;
 	}
-	public void canVote () {
-		if (age >= 18) {
-			System.out.println("This person can vote.");
+	public boolean canVote(int year) {
+		if (age >= 18 && year % 4 == 0) {
+			return true;
 		}
-		else {
-			System.out.println("This person cannot vote.");
-		}
+		return false;
 	}
-	public void tetanusShot () {
-		if (age % 4 == 0) {
-			System.out.println("This person needs a tetanus shot.");
-		}
-		else {
-			System.out.println("This person doesn't need a tetanus shot.");
-		}
+	public void setName(String newName) {
+		name = newName;
 	}
-	public void toddler () {
-		if (age < 4 && gender == 'b') {
-			System.out.println("Toddler boy.");
-		}
-		else if (age < 4 && gender == 'g') {
-			System.out.println("Toddler girl.");
-		}
-		else {
-			System.out.println("Not a toddler.");
-		}
-	}
-	public void movieDiscounts () {
-		if (age < 12 || age > 65) {
-			System.out.println("Movie discount.");
-		}
-		else {
-			System.out.println("No movie discount.");
-		}
-	}
-	public void teenager () {
-		if (age >= 12 && age <= 18) {
-			System.out.println("Teenager.");
-		}
-		else {
-			System.out.println("Not a teenager.");
-		}
+	public String toString() {
+		return "Name: " + name + ", Age: " + age + ", Vote: " + canVote(2020);
 	}
 	public static void main(String[] args) {
+		Human Tony = new Human(18, 150, "Tony");
+		System.out.println(Tony);
 		
-		Human tester = new Human();
-		tester.setAge(12);
-		tester.setGender('g');
-		tester.canVote();
-		tester.tetanusShot();
-		tester.toddler();
-		tester.movieDiscounts();
-		tester.teenager();
 	}
 }
