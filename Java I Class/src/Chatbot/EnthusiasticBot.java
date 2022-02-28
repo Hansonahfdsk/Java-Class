afd1352945531f42ff2f727f034982a4175ef1b7
hansonh <hansonh@localhost>
2022-02-27 19:26:38 -0500

    bots

--- Java I Class/src/Chatbot/EnthusiasticBot.java
+++ Java I Class/src/Chatbot/EnthusiasticBot.java
@@ -1,5 +1,41 @@
 package Chatbot;
 
-public class EnthusiasticBot {
+public class EnthusiasticBot extends ChatBot {
 
+	@Override
+	public void sayHi() {
+		System.out.println("Hi!!!!!");
+		
+	}
+
+	@Override
+	public void sayBye() {
+		System.out.println("Bye!!!!!");
+		
+	}
+
+	@Override
+	public void startConversation() {
+		System.out.println("I hope you are doing well!!!");
+		
+	}
+
+	@Override
+	public void askFirstQuestion() {
+		System.out.println("How are you!!!!");
+		
+	}
+
+	@Override
+	public void askSecondQuestion() {
+		System.out.println("How is your day!!!!");
+		
+	}
+
+	@Override
+	public void askThirdQuestion() {
+		System.out.println("What are your grades!!!");
+		
+	}
+	
 }
--- Java I Class/src/Chatbot/SarcasticBot.java
+++ Java I Class/src/Chatbot/SarcasticBot.java
@@ -1,5 +1,41 @@
 package Chatbot;
 
-public class SarcasticBot {
+public class SarcasticBot extends ChatBot {
 
+	@Override
+	public void sayHi() {
+		System.out.println("Hi~");
+		
+	}
+
+	@Override
+	public void sayBye() {
+		System.out.println("Bye~");
+		
+	}
+
+	@Override
+	public void startConversation() {
+		System.out.println("I hope you are doing well");
+		
+	}
+
+	@Override
+	public void askFirstQuestion() {
+		System.out.println("How are you, huh?");
+		
+	}
+
+	@Override
+	public void askSecondQuestion() {
+		System.out.println("How are classes? Hehe");
+		
+	}
+
+	@Override
+	public void askThirdQuestion() {
+		System.out.println("What are your grades?");
+		
+	}
+	
 }
